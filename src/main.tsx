a5da958fef23ed97ba3a23b94fc716ff4e55e619
bruno adriano <brunoadria.contato@gmail.com>
2025-11-27 15:02:16 -0300

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,20 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+
+// ! Importa o arquivo de estilos global
+import '@/styles/main.scss';
+
+import App from './App.tsx';
+
+const root = document.getElementById('root');
+
+if (root) {
+  createRoot(root).render(
+    <StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </StrictMode>,
+  )
+}
